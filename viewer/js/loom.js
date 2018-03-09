da04d7ab23ad0823f3b97383e8c55c6082a301dd
Mohammad Raji <moa@dragonfli.es>
2018-03-09 12:02:18 -0500

    added working tablaeu example and loom.js version

--- viewer/js/loom.js
+++ viewer/js/loom.js
@@ -3,10 +3,7 @@ class Viewer
     constructor()
     {
         this.targets = {}; // a linear representation of the targets
-        /*fetch(config_filename)
-            .then(response => response.text())
-            .then(configs => this.configs = configs)*/
-        //this.init(this.configs);
+        this.fps = 29;
     }
 
     load(config_filename)
@@ -38,53 +35,58 @@ class Viewer
         let parallel_video_el = 
             document.querySelector("#parallel_video>video");
 
-        var self = this;
         parallel_video_el.addEventListener("seeked", function(){
             var canvas = document.getElementById("parallel_video_canvas");
             var context = canvas.getContext("2d");
-            var width = self.config.window.width;
-            var height = self.config.window.height;
+            var width = this.config.window.width;
+            var height = this.config.window.height;
             context.drawImage(parallel_video_el, 0, 0, width, height);
             var pixels = context.getImageData(0, 0, width, height);
 
-            function fastSimilar(a, b)
-            {
-                if (Math.abs(a - b) < 20)
-                {
-                    return true;
-                }
-                return false;
-            }
+            pixels = this.removeMagenta(pixels);
 
-            for (var i = 0; i < pixels.data.length; i += 4)
-            {
-                var r = pixels.data[i + 0],
-                    g = pixels.data[i + 1],
-                    b = pixels.data[i + 2];
-                
-                if (fastSimilar(r, 255) && 
-                        fastSimilar(g, 0) && 
-                        fastSimilar(b, 255))
-                {
-                    pixels.data[i + 3] = 0;
-                }
-                else
-                {
-                    var magenta = [60.3199336, 98.254218, -60.842984];     
-                    var other = rgb2lab([r, g, b]);
-                    if (deltaE(magenta, other) < 20)
-                    {
-                        pixels.data[i + 3] = 0;
-                    }
-                }
-
-            } 
             context.putImageData(pixels, 0, 0);
-        });
+        }.bind(this));
 
         // ...
 
         this.setup();
+    }
+
+    removeMagenta(pixels)
+    {
+        function fastSimilar(a, b)
+        {
+            if (Math.abs(a - b) < 20)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        for (var i = 0; i < pixels.data.length; i += 4)
+        {
+            var r = pixels.data[i + 0],
+                g = pixels.data[i + 1],
+                b = pixels.data[i + 2];
+            
+            if (fastSimilar(r, 255) && 
+                    fastSimilar(g, 0) && 
+                    fastSimilar(b, 255))
+            {
+                pixels.data[i + 3] = 0;
+            }
+            else
+            {
+                var magenta = [60.3199336, 98.254218, -60.842984];     
+                var other = rgb2lab([r, g, b]);
+                if (deltaE(magenta, other) < 20)
+                {
+                    pixels.data[i + 3] = 0;
+                }
+            }
+        } 
+        return pixels;
     }
 
     setupVideo(element)
@@ -121,40 +123,104 @@ class Viewer
         return handler;
     }
 
+    // determine if state can change and change it if it can
+    changeState(target_el, video_name)
+    {
+        let frame = parseInt($(target_el).attr("data-frame")); // frame for this interaction
+        let target = this.targets[frame];
+        if (this.findChild(target, this.current_state) != null 
+                || this.findSibling(target, this.current_state) != null
+                || this.findChild(target, this.config) != null
+                || target.name == 'root')
+        {
+            this.current_state = target;
+            let video = videojs(video_name); // video layer to change
+            video.currentTime(frame / this.fps);
+        }
+    }
+
+    // tries to find a target state in the immediate children of another 
+    // based on its frame number
+    findChild(needle, haystack)
+    {
+        if (!haystack.hasOwnProperty('children'))
+        {
+            // then it's a leaf node
+            return null;
+        }
+        for (var i = 0; i < haystack.children.length; i++)
+        {
+            if (haystack.children[i].frame_no == needle.frame_no)
+            {
+                return haystack.children[i];
+            }
+        }   
+        return null;
+    }
+
+    // tries to find a target state (needle) in the siblings of 
+    // another (other) based on its frame number
+    findSibling(needle, other)
+    {
+        // find parent first so we can access the children
+        let par = this.findByName(other.parent);
+        if (par == null || !par.hasOwnProperty('children'))
+        {
+            return null;
+        }
+
+        for (var i = 0; i < par.children.length; i++)
+        {
+            if (par.children[i].frame_no == needle.frame_no)
+            {
+                return par.children[i];
+            }
+        }
+        return null;
+    }
+
+    // finds a target by its name
+    findByName(name)
+    {
+        for (var i = 0; i < Object.keys(this.targets).length; i++)
+        {
+            if (this.targets[i]['name'] == name)
+            {
+                return this.targets[i];
+            }   
+        }
+        return null;
+    }
+
     traverse(target)
     {
         if (target.name != 'root')
         {
-            var fps = 29;
             if (target.type == 'parallel' && target.actor == 'button')
             {
                 let handler = this.createInteractionHandler(target);
-                this.targets[target.frame_no] = handler;
-                handler.on("click", function(){
-                    let frame = parseInt($(this).attr("data-frame"));
-                    let video = videojs("parallel_video");
-                    video.currentTime(frame / fps);
-                });
+                this.targets[target.frame_no] = target;
+                handler.on("click", function(e){
+                    this.changeState(e.target, "parallel_video");
+                }.bind(this));
             }
             if (target.type == 'linear' && target.actor == 'button')
             {
                 let handler = this.createInteractionHandler(target);
-                this.targets[target.frame_no] = handler;
-                handler.on("click", function(){
-                    let frame = parseInt($(this).attr("data-frame"));
-                    let video = videojs("video");
-                    video.currentTime(frame / fps);
-                });
+                this.targets[target.frame_no] = target;
+                handler.on("click", function(e){
+                    this.clearParallelVideoCanvas();
+                    this.changeState(e.target, "video");
+                }.bind(this));
             }
             if (target.type == 'linear' && target.actor == 'hover')
             {
                 let handler = this.createInteractionHandler(target);
-                this.targets[target.frame_no] = handler;
-                handler.on("mouseover", function(){
-                    let frame = parseInt($(this).attr("data-frame"));
-                    let video = videojs("video");
-                    video.currentTime(frame / fps);
-                });
+                this.targets[target.frame_no] = target;
+                handler.on("mouseover", function(e){
+                    this.clearParallelVideoCanvas();
+                    this.changeState(e.target, "video");
+                }.bind(this));
             }
         }
         if (target.hasOwnProperty('children'))
@@ -168,6 +234,7 @@ class Viewer
 
     setup()
     {
+        this.current_state = this.config;
         this.traverse(this.config);
     }
 
@@ -193,7 +260,8 @@ class Viewer
     }
 }
 
+let viewer = null;
 $(document).ready(function(){
-    let viewer = new Viewer();
+    viewer = new Viewer();
     viewer.load("config.json");
 });
