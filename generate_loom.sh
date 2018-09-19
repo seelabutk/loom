2b5cf3f1a3d54aaeb88fe8eaede6919056f77ae5
Mohammad Raji <moa@dragonfli.es>
2018-09-19 16:33:09 -0400

    Added random name to exports to prevent disk cache on OSX

--- generate_loom.sh
+++ generate_loom.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-$1
-ffmpeg -y -framerate 30 -i images/%05d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p viewer/videos/loom.mp4
+#$1
+
+name=$RANDOM
+ffmpeg -y -framerate 30 -i images/%05d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p viewer/videos/$name.mp4
+sed -i '' "s/src=\"videos\/[0-9]*\.mp4\"/src=\"videos\/$name.mp4\"/g" viewer/loom.html
 
 #ffmpeg -y -framerate 30 -i parallel_images/%05d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p  -vf scale=$1 viewer/videos/parallel_loom.mp4
