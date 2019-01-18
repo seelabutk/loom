e08e5be3fbabde40db13a6b3f8dad944e5f09f23
Mohammad Raji <moa@dragonfli.es>
2019-01-18 18:17:41 -0500

    added encryption and decryption mechanisms

---
+++ viewer/decrypt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./encrypt <filename> <query> <pass>"
+    exit;
+fi
+
+filename=$1
+query=$2
+pass=$3
+ffmpeg -i $filename temp/frame_%05d.jpg 
+cat config.json | 
+    jq -j '.. | 
+    select(.description?) | 
+    .frame_no,",",.description, "\n"' | 
+    grep $query | 
+    cut -d',' -f1 | 
+    xargs -I{} printf %05d"\n" {} |
+    xargs -I{} sh -c 'openssl enc -aes-256-cbc -pass pass:$pass -d -p -in temp/frame_{}.enc -out temp/frame_{}.jpg;'
+
+ffmpeg -y -framerate 30 -i temp/frame_%05d.jpg -c:v libx264 \
+    -profile:v high -crf 20 -pix_fmt yuv420p $filename
+
